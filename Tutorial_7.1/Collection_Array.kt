a2091ac00bb094c5648e93d2d8ed74ac124239ff
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2024-11-26 14:02:45 +0100

    Update

---
+++ Tutorial_7.1/Collection_Array.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val solarSystem = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+
+    solarSystem[2] = "Little Earth"
+
+    println(solarSystem[3])
+}
---
+++ Tutorial_7.1/Collection_Lists.kt
@@ -0,0 +1,36 @@
+fun listOfFunction() {
+    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    println(solarSystem.get(3))
+
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+
+    for (planet in solarSystem) {
+        println(planet)
+    }
+}
+
+fun mutableListOf() {
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+
+    solarSystem[3] = "Future Moon"
+
+    println(solarSystem[3])
+    println(solarSystem[9])
+
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
+}
+
+
+fun main() {
+    listOfFunction()
+    mutableListOf()
+}
